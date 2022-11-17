eb268bfc36a2868ca4381b127f6969606922f9fc
Welt <bweltron@gmail.com>
2022-11-17 06:25:24 -0600

    Working with strings

---
+++ Learn/strings.py
@@ -0,0 +1,7 @@
+#working with strings, manipulating cases
+fName = "weltron"
+lName = "bitange"
+name = fName + " " + lName
+print(name.title()) #changes first letter of each word to upper case
+print(name.upper()) #changes entire string to upper case
+print(name.lower()) #changes to lower case
