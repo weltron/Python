ba5489cef8f1dcd231794f435452eb33d07b1f1d
Welt <bweltron@gmail.com>
2023-03-07 12:53:38 -0600

    Refractored Day 6/100

--- 100DaysOfPython/Day6/maze.py
+++ 100DaysOfPython/Day6/maze.py
@@ -4,15 +4,17 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
+
+while front_is_clear():
+    move()
+turn_left()
      
 while not at_goal():
-    if front_is_clear():
-        move()
-    elif right_is_clear():
+    if right_is_clear():
         turn_right()
+        move()
+    elif front_is_clear():
         move()
     else:
         turn_left()
     
-   
